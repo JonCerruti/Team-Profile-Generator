06ad411d8636e29508b507edf7e8ef3646f975a8
Jon <jonathancerruti@gmail.com>
2022-08-01 01:34:00 -0700

    psuedo code for index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// inquirer
+// fs
+// require job rols js
+// function to initialize app
+// function to add team member 
+// inquire prompt questions 
+// function that adds in html
+// function that adds team members to html card
+// function that writes html file
